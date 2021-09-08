f1a92f269d7c29f90a8ea51f9dcfec46556da772
= <husamsaleem707@gmail.com>
2021-09-07 22:14:45 -0700

    Test size added

--- Knn.py
+++ Knn.py
@@ -16,7 +16,7 @@ def split_input_data(data):
         y.append(c[3])
     
     x.pop(0)
-    X_train, X_test, y_train, y_test = train_test_split(x, y)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
     return X_train, X_test, y_train, y_test
 
 def display_contours(X_train, y_train, classifier, number_of_neighbors):
